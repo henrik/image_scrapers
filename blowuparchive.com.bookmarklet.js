3f4bb4463c8291f5918d3656774e5fee5923f5ed
Henrik N <henrik@nyh.se>
2013-01-30 20:32:39 +0100

    Add criterion.com bookmarklet.

--- blowuparchive.com.bookmarklet.js
+++ blowuparchive.com.bookmarklet.js
@@ -1,3 +1,4 @@
 // Replaces URL with image URL.
 // E.g. on http://blowuparchive.com/details.php?gal=BLOW-UP&image=3.%20Veruschka%20in%20_Blow-Up_%20(1966)%20by%20Arthur%20Evans.jpg
+
 location.href = document.getElementsByTagName("embed")[0].src.replace(/\.com\/.*&img=/, ".com/");
---
+++ criterion.com.bookmarklet.js
@@ -0,0 +1,4 @@
+// Make lightbox images saveable. Trigger after opening lightbox.
+// E.g. on http://www.criterion.com/current/posts/2038-cast-away
+
+document.getElementById("hoverNav").style.display = "none"; return false
