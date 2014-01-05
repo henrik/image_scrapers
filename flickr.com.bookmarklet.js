acaf3a99a38f0e1d757558c8e25353c2885b49a8
Henrik Nyh <henrik@nyh.se>
2014-01-05 23:18:37 +0100

    Fix Flickr bookmarklet to find largest version.

--- flickr.com.bookmarklet.js
+++ flickr.com.bookmarklet.js
@@ -1,4 +1,6 @@
 // Trigger on a photo page, e.g.
 // http://www.flickr.com/photos/americanvintagehome/3655377927/in/photostream/
+// or
+// http://www.flickr.com/photos/tikitacky/8026494322/in/photostream/
 
-location.href=document.getElementsByName("og:image")[0].content;
+var is=JSON.parse(document.body.innerHTML.match(/"sizes":({.*}),"secret"/)[1]); var url = "", max = 0; for (var k in is) { var x = is[k]; if (x.width>max) url = x.url; }; location.href=url
