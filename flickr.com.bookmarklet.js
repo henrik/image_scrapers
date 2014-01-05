1ba3fd92491bad2119da5bf7871eca14e33a86b6
Henrik Nyh <henrik@nyh.se>
2014-01-05 23:24:40 +0100

    Fix Flickr bookmarklet to actually find largest image

--- flickr.com.bookmarklet.js
+++ flickr.com.bookmarklet.js
@@ -3,4 +3,4 @@
 // or
 // http://www.flickr.com/photos/tikitacky/8026494322/in/photostream/
 
-var is=JSON.parse(document.body.innerHTML.match(/"sizes":({.*}),"secret"/)[1]); var url = "", max = 0; for (var k in is) { var x = is[k]; if (x.width>max) url = x.url; }; location.href=url
+var is=JSON.parse(document.body.innerHTML.match(/"sizes":({.*}),"secret"/)[1]); var url = "", max = 0; for (var k in is) { var x = is[k]; if (x.width>max) { url = x.url; max = x.width; } }; location.href=url
